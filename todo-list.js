8a6f2a8b03311f9a93f824e0744d2aec4bc05cbd
NadimElMasry <nadimmasry@ymail.com>
2025-04-23 21:16:29 +0200

    Add loop (inside new function) with HTML generator code to display inside new <div> element

--- todo-list.js
+++ todo-list.js
@@ -1,12 +1,26 @@
 const todoList = [];
 
+renderTodoList();
+
+function renderTodoList() {
+  let todoElementHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoElementHTML += html;    
+  }
+  
+  document.querySelector('.js-todo-list').innerHTML = todoElementHTML;
+}
+
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
 
   todoList.push(name);
 
-  console.log(todoList);
-
   inputElement.value = '';
+
+  renderTodoList();
 }
