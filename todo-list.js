3f638abf2a1548aef7773514ac0b8966a40e8122
NadimElMasry <nadimmasry@ymail.com>
2025-05-02 18:01:45 +0300

    Update to save to localStorage and handle keydown event; add validation step to skip look in case broken data is saved to localStorage

--- todo-list.js
+++ todo-list.js
@@ -1,33 +1,6 @@
-const todoList = [{
-  name: 'make dinner',
-  dueDate: '2022-12-22'
-}, {
-  name: 'wash dishes',
-  dueDate: '2022-12-22'
-}];
+const todoList = JSON.parse(localStorage.getItem('todoList')) || [];
 
 renderTodoList();
-
-function renderTodoList() {
-  let todoElementHTML = '';
-
-  for (let i = 0; i < todoList.length; i++) {
-    const todoObject = todoList[i];
-    // const name = todoObject.name;
-    // const dueDate = todoObject.dueDate;
-    const { name, dueDate } = todoObject;
-    const html = `
-    <div>${name}</div>
-    <div>${dueDate}</div> 
-    <button onclick="
-      todoList.splice(${i}, 1);
-      renderTodoList();
-    " class="delete-todo-button">Delete</button>
-    `;
-    todoElementHTML += html;
-  }  
-  document.querySelector('.js-todo-list').innerHTML = todoElementHTML;
-}
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
@@ -46,4 +19,44 @@ function addTodo() {
   inputElement.value = '';
 
   renderTodoList();
+
+  saveToStorage();
+}
+
+function renderTodoList() {
+  let todoElementHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+
+    if (!todoObject || typeof todoObject !== 'object' || !todoObject.name) {
+      continue;
+    } /* In case broken data was accidentally saved in localStorage (if the item is not a valid object or has no name), this code skips the loop. */
+    
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate;
+    const { name, dueDate } = todoObject;
+
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div> 
+    <button onclick="
+      todoList.splice(${i}, 1);
+      renderTodoList();
+      saveToStorage();
+    " class="delete-todo-button">Delete</button>
+    `;
+    todoElementHTML += html;
+  }  
+  document.querySelector('.js-todo-list').innerHTML = todoElementHTML;
+}
+
+function saveToStorage() {
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+}
+
+function handleKeydown(event) {
+  if (event.key === 'Enter') {
+    addTodo();
+  }
 }
