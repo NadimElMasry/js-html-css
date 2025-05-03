fc302182b9ad991d981536e0a96769028acc37d4
NadimElMasry <nadimmasry@ymail.com>
2025-05-03 23:19:08 +0300

    Add autoPlay() function; declare global flag variable and interval ID variable

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,6 +6,23 @@ let score = JSON.parse(localStorage.getItem('score')) || {
 
 updateScoreElement();
 
+let isAutoPlaying = false;
+
+let intervalID;
+  
+function autoPlay() {
+  if (!isAutoPlaying) {
+    intervalID = setInterval(function() {
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    }, 1000);
+    isAutoPlaying = true;
+  } else {
+    clearInterval(intervalID);
+    isAutoPlaying = false;
+  }
+}
+
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
 
